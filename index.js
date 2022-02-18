86592c7bfed266924941315dfa5f6770f928f0fb
hannah AB <hannah.allisonbatt@gmail.com>
2022-02-18 10:49:17 +0800

    classes complete

---
+++ index.js
@@ -0,0 +1,32 @@
+// inquirer questions for employee
+
+//do you want to add another person?
+
+//run code again
+
+//for each 
+
+//create html card
+
+//join them togther
+
+//generate object function to create strings
+
+// join the strings and create an html with constructor
+
+
+
+////├── __tests__/             //jest tests
+//│   ├── Employee.test.js
+//│   ├── Engineer.test.js
+//│   ├── Intern.test.js
+//│   └── Manager.test.js
+//├── dist/                  // rendered output (HTML) and CSS style sheet      
+//├── lib/                   // classes
+//├── src/                   // template helper code 
+//├── .gitignore             // indicates which folders and files Git should ignore
+//├── index.js               // runs the application
+//└── package.json   
+
+
+
