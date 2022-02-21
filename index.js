2d90c7770c79ddb6ff6efce3b6927f7d10e94381
hannah AB <hannah.allisonbatt@gmail.com>
2022-02-21 09:49:16 +0800

    minor edits

--- index.js
+++ index.js
@@ -12,73 +12,71 @@ const renderManagerCard = require("./lib/createCards");
 const fs = require('fs');
 const employeeCards = [];
 
-function acquireEmployeeInfo(){
+function acquireEmployeeInfo() {
 
     inquirer.prompt([
-    {
-        type: 'list',
-        message: 'please select team members role',
-        choices: ['manager', 'engineer', 'intern'],
-        name: 'role',
-    },
-    {
-        type: 'input',
-        message: 'please type team members full name',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'please type team members ID number',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'please type team members email address',
-        name: 'email',
-    },
-    {
-        type: 'input',
-        message: 'please type team members office number',
-        name: 'officeNumber',
-        when:(answers) => answers.role === "manager",
-    },
-    {
-        type: 'input',
-        message: 'please type team members github link',
-        name: 'github',
-        when:(answers) => answers.role === "engineer",
-    },
-    {
-        type: 'input',
-        message: 'please type team members school name',
-        name: 'school',
-        when:(answers) => answers.role === "intern",
-    },
-    {
-        type: 'confirm',
-        message: 'would you like to add another team member?',
-        name: 'addNew'
-    },
+        {
+            type: 'list',
+            message: 'please select team members role',
+            choices: ['manager', 'engineer', 'intern'],
+            name: 'role',
+        },
+        {
+            type: 'input',
+            message: 'please type team members full name',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'please type team members ID number',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'please type team members email address',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'please type team members office number',
+            name: 'officeNumber',
+            when: (answers) => answers.role === "manager",
+        },
+        {
+            type: 'input',
+            message: 'please type team members github link',
+            name: 'github',
+            when: (answers) => answers.role === "engineer",
+        },
+        {
+            type: 'input',
+            message: 'please type team members school name',
+            name: 'school',
+            when: (answers) => answers.role === "intern",
+        },
+        {
+            type: 'confirm',
+            message: 'would you like to add another team member?',
+            name: 'addNew'
+        },
 
-]).then((answers)=>{
-    switch(answers.role) {
-        case "engineer":
-          engineers.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
+    ]).then((answers) => {
+        if (answers.role == "engineer") {
 
-          break;
-        case "manager":
+            engineers.push(new Engineer(answers.name, answers.id, answers.email, answers.github));
+
+        }
+        if (answers.role == "manager") {
             managers.push(new Manager(answers.name, answers.id, answers.email, answers.github));
-          break;
-          case "intern":
+        }
+        if (answers.role == "intern") {
             interns.push(new Intern(answers.name, answers.id, answers.email, answers.github));
-    }
-
-    switch(answers.addNew){
-        case ("y"):
+        }
+        if (answers.addNew == "yes") {
             acquireEmployeeInfo();
-            break;
-            case ("n"):
-            
+        }
+        if (answers.addNew == "no") {
+
             for (let i = 0; i < engineers.length; i++) {
                 const newEngineer = engineers[i];
 
@@ -96,18 +94,20 @@ function acquireEmployeeInfo(){
             }
 
             fs.writeFile(".././dist/index.html", renderHtml(employeeCards));
-              
-                  
-            
+
+            console.log(employeeCards);
+            console.log(managers);
+            console.log(interns);
+
         }
-    
-});
+
+
+    });
 
 }
 
-function init(){
+function init() {
     acquireEmployeeInfo();
 }
 
 init();
-
